0d7c543a124dc1efb86b19c5817d53a56ad7e955
James <james.nolan38@mail.dcu.ie>
2018-12-16 04:26:48 +0000

    Added DataType.java

---
+++ assignment_2/DataType.java
@@ -0,0 +1,17 @@
+public enum DataType{
+	SimpleNode,
+	Program,
+	Function,
+	FunctionRet,
+	VariableDecl,
+	ConstantDecl,
+	Integer,
+	Boolean,
+	TypeUnknown,
+	Identifer,
+	Main,
+	Statement,
+	Expression,
+	Assignment
+}
+//needed in semantic check
